248f6342a1b333eba26ebff82486cf3885e55a91
Sean Swonger <sdswonger@gmail.com>
2025-10-27 21:57:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "permitflow-yywjs",
+  "appId": "1:941819701655:web:3a21b2aab77668e2e93cd2",
+  "apiKey": "AIzaSyA2hW6M9EUiLXZ6hsXbBCxSK_Xlf5B1uOM",
+  "authDomain": "permitflow-yywjs.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "941819701655"
+};
